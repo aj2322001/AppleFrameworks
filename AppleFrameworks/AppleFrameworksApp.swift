96bf326bdd8a26d5f478ff938e69bdb50d168726
Archit Joshi <archit.joshi@anarock.com>
2025-09-27 17:50:18 +0530

    Initial Commit

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Archit  Joshi on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
